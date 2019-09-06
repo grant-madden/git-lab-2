1c930b6cb27dccb480543964b71ea91de3db33d6
Grant Madden <gm197018@ohio.edu>
2019-09-06 17:07:08 -0400

    Program Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Grant Madden
+ *        Date: 9/6/2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*print statement*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+    
+}// main
